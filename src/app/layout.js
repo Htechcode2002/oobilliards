966a9276956ebab41387afeada24b2cff7f7c2c7
ck <htechcodesolution888@gmail.com>
2025-10-05 19:18:44 +0800

    save

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,7 +36,7 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }) { 
   return (
     <html lang="en">
       <body
